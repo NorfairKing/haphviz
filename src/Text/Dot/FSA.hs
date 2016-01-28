ab066849ddfe18bd15a9f1767ea4b4a2a22aad15
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2016-01-28 22:02:48 +0100

    Added the graph class to allow for rendering of arbitrary graph-like datatypes

--- src/Text/Dot/FSA.hs
+++ src/Text/Dot/FSA.hs
@@ -1,15 +1,17 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
 -- | Easy FSA visualisation
 module Text.Dot.FSA where
 
 import           Text.Dot
+import           Text.Dot.Class
 
-import           Control.Monad (forM, forM_, when)
-import           Data.Maybe    (fromMaybe, isNothing)
+import           Control.Monad  (forM, forM_, when)
+import           Data.Maybe     (fromMaybe, isNothing)
 
 import           Data.Text
-import qualified Data.Text     as T
-import qualified Data.Text.IO  as T
+import qualified Data.Text      as T
+import qualified Data.Text.IO   as T
 
 -- | An easy way to generate an FSA visualization
 --
@@ -35,47 +37,55 @@ import qualified Data.Text.IO  as T
 -- >     1 -> 1 [label=<q>];
 -- >     1 -> 2 [label=<p>];
 -- > }
---
---
-fsaGraph :: [Text] -- ^ Set of states
-          -> Text -- ^ Initial state
-          -> [Text] -- ^ Accepting states
-          -> [(Text, Text, Text)] -- ^ Edges: From, To, Symbol
-          -> DotGraph
-fsaGraph states initial accepting edges = graph_ directed $ do
-    nodeDec [width =: "0", height =: "0"] -- Nodes as small as possible
-    rankdir leftRight
 
-    stateNodes <- forM states $ \s -> do
-        n <- newNode
-        genNode n $
-            if s `elem` accepting
-                then [label =: s, shape =: "doublecircle"]
-                else [label =: s]
-        return (s, n)
+data FSA = FSA {
+      fsaStates    :: [Text] -- ^ Set of states
+    , fsaInitial   :: Text -- ^ Initial state
+    , fsaAccepting :: [Text] -- ^ Accepting states
+    , fsaEdges     :: [(Text, Text, Text)] -- ^ Edges: From, To, Symbol
+    } deriving (Show, Eq)
 
-    -- Check that accepting states are actually states
-    forM_ accepting $ \s -> do
-        when
-            (isNothing $ lookup s stateNodes)
-            (error $ "Accepting state is not in the set of states: " ++ T.unpack s)
+data FSARenderConfig = FSARenderConfig
 
-    -- Draw an edge from an invisible state to the initial state
-    case lookup initial stateNodes of
-        Nothing -> error "Initial state is not in the set of states"
-        Just initialNode -> do
+instance Graph FSA FSARenderConfig where
+    defaultGenConfig = FSARenderConfig
+    genGraph _ (FSA states initial accepting edges) = do
+        nodeDec [width =: "0", height =: "0"] -- Nodes as small as possible
+        rankdir leftRight
+
+        stateNodes <- forM states $ \s -> do
             n <- newNode
-            genNode n ["style" =: "invis"]
-            n --> initialNode
+            genNode n $
+                if s `elem` accepting
+                    then [label =: s, shape =: "doublecircle"]
+                    else [label =: s]
+            return (s, n)
 
-    -- Draw the edges
-    forM_ edges $ \(from, to, symbol) -> do
-        let fromNode = fromMaybe
-                    (error $ "From node not found: " ++ T.unpack from)
-                    (lookup from stateNodes)
-        let toNode = fromMaybe
-                    (error $ "To node not found: " ++ T.unpack to)
-                    (lookup to stateNodes)
-        genEdge fromNode toNode [label =: symbol]
+        -- Check that accepting states are actually states
+        forM_ accepting $ \s -> do
+            when
+                (isNothing $ lookup s stateNodes)
+                (error $ "Accepting state is not in the set of states: " ++ T.unpack s)
+
+        -- Draw an edge from an invisible state to the initial state
+        case lookup initial stateNodes of
+            Nothing -> error "Initial state is not in the set of states"
+            Just initialNode -> do
+                n <- newNode
+                genNode n ["style" =: "invis"]
+                n --> initialNode
+
+        -- Draw the edges
+        forM_ edges $ \(from, to, symbol) -> do
+            let fromNode = fromMaybe
+                        (error $ "From node not found: " ++ T.unpack from)
+                        (lookup from stateNodes)
+            let toNode = fromMaybe
+                        (error $ "To node not found: " ++ T.unpack to)
+                        (lookup to stateNodes)
+            genEdge fromNode toNode [label =: symbol]
 
 
+
+fsaGraph :: FSA -> DotGraph
+fsaGraph g = graph_ directed $ genGraph FSARenderConfig g
